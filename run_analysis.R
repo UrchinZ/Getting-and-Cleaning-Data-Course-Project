5234c00ab16edff55ea1abb9a87934271c0e4e7d
Judy Z <2j.zy.zhang@gmail.com>
2021-05-22 16:41:51 -0400

    Completed assignment logic
    rt

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+library(dplyr)
 # 1. Merges the training and the test sets to create one data set.
 ### Get data values
 path <- "./UCI HAR Dataset/"
@@ -22,10 +23,19 @@ subject_name <- "subject"
 X_features_names <- read.table("./UCI HAR Dataset/features.txt")$V2
 y_name <- "activity_label"
 colnames(df) <- c(subject_name, X_features_names, y_name)
-print("Train and test dataframe merged")
+print("Dataframe merged")
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+interest <- X_features_names[grep("mean|std", X_features_names)]
+length(interest)
+df <- df[,c(subject_name, interest, y_name)]
 # 3. Uses descriptive activity names to name the activities in the data set
+activity_df <- read.table("./UCI HAR Dataset/activity_labels.txt")
+colnames(activity_df) <- c(y_name,'activity')
+df <- left_join(df, activity_df, by=c(y_name))
+df <- select(df, -y_name)
 # 4. Appropriately labels the data set with descriptive variable names. 
+# Already done in step 1
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidy_df <- df %>% group_by(subject, activity) %>% summarise(across(everything(), mean))
 
