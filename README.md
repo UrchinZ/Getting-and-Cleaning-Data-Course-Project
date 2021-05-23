5f0275f1b65b51c7a3946ebd37d54961362ca094
Judy Z <2j.zy.zhang@gmail.com>
2021-05-23 11:19:23 -0400

    Reformatted R script, Added README description
    + package installation
    + file download and unzip
    -> reordered script comments
    + descriptions of repository files

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Getting and Cleaning Data Course Project
  Preparing data collected from the accelerometers from the Samsung Galaxy S smartphone for future analysis
- Full description of dataset can be found [here] (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+ Full description of source dataset can be found [here] (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+# Prerequisite:
+- Please ensure there are more than 100Mb of memory available
+- R is installed. script will try to install dplyr package. 
+
+# Files
+- README.txt: explains how all of the scripts work and how they are connected
+- CodeBook.md: describes the variables, the data, and any transformations or work that were performed to clean up the data
+- run_analysis.R: prepares a tidy data set from source and saves it in working directory as "tidy_df.txt"
 
  
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,15 @@
+install.packages("dplyr")
 library(dplyr)
-# 1. Merges the training and the test sets to create one data set.
-### Get data values
+
+# Download dataset from source. Make sure there are more than 100 Mb in your space 
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if (!file.exists("./UCI HAR Dataset")){
+    download.file(url, dest="./dataset.zip", mode="wb")
+    unzip ("./dataset.zip")
+    file.remove("./dataset.zip")
+}
+# Format dataseet
+## Read in data values
 path <- "./UCI HAR Dataset/"
 type <- c("train", "test")
 subject <- c("subject", "X", "y")
@@ -18,24 +27,28 @@ for (t in type){
     df <- rbind(df, df_type)
 }
 
-### Get data feature names
+## Appropriately labels the data set with descriptive variable names.
 subject_name <- "subject"
 X_features_names <- read.table("./UCI HAR Dataset/features.txt")$V2
 y_name <- "activity_label"
 colnames(df) <- c(subject_name, X_features_names, y_name)
 print("Dataframe merged")
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
 interest <- X_features_names[grep("mean|std", X_features_names)]
 length(interest)
 df <- df[,c(subject_name, interest, y_name)]
-# 3. Uses descriptive activity names to name the activities in the data set
+
+# Uses descriptive activity names to name the activities in the data set
 activity_df <- read.table("./UCI HAR Dataset/activity_labels.txt")
 colnames(activity_df) <- c(y_name,'activity')
 df <- left_join(df, activity_df, by=c(y_name))
 df <- select(df, -y_name)
-# 4. Appropriately labels the data set with descriptive variable names. 
-# Already done in step 1
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidy_df <- df %>% group_by(subject, activity) %>% summarise(across(everything(), mean))
 
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidy_df <- df %>% group_by(subject, activity) %>% summarise(across(everything(), mean))
+write.table(tidy_df, file="./tidy_df.txt")
+# result <- read.table("./tidy_df.txt")
+
+# Cleanup
+unlink("./UCI HAR Dataset", recursive=TRUE) 
